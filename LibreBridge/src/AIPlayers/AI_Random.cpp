14d8529c2ddb08bd09a95cff2c51145c3d4f85c8
joerenman <41649388+joerenman@users.noreply.github.com>
2020-12-18 14:33:56 +0000

    Add files via upload

---
+++ LibreBridge/src/AIPlayers/AI_Random.cpp
@@ -0,0 +1,22 @@
+#include "AI_Random.hpp"
+#include "../Misc.hpp"
+
+using namespace std;
+
+Card AI_Random::chooseCardToPlay(Suit firstSuit, vector<Card> dummyHand)
+{
+	return playRandomCard(firstSuit, dummyHand);
+}
+
+void AI_Random::makeBid(BetType& betType, Suit& suit, uint8_t& level, vector<Bid> bidWar)
+{
+	betType = Pass;
+	// If everybody passed so far, 20% chance to make a random level 1 bet
+	for (auto &bid : bidWar) if(bid.getBetType() == Normal) level = bid.getLevel();
+	if(!level && randomUint8(0, 4))
+	{
+		betType = Normal;
+		level = 1;
+		suit = Suit(randomUint8(1, 5));
+	}
+}
